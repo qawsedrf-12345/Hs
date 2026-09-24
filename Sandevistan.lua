410a82d20e858ddfc8a387d67f9bed33f60e5cdc
qawsedrf-12345 <alexandrebarbosaduarte2011@gmail.com>
2026-09-23 22:56:44 -0300

    Atualizar o Sandevistan.lua

--- Sandevistan.lua
+++ Sandevistan.lua
@@ -108,7 +108,7 @@ end
 --=============================================================
 local TOGGLE_KEY            = Enum.KeyCode.F
 local NORMAL_SPEED          = 16
-local BOOSTED_SPEED         = 110
+local BOOSTED_SPEED         = 105
 local SANDEVISTAN_DURATION  = 3.5
 local CLONE_INTERVAL        = 0.14
 local MAX_CLONES            = 25
