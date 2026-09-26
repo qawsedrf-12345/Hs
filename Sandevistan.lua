82ea9a610d42693b19cc519a8a2f57be719101d3
qawsedrf-12345 <alexandrebarbosaduarte2011@gmail.com>
2026-09-26 10:29:21 -0300

    Atualizar o Sandevistan.lua

--- Sandevistan.lua
+++ Sandevistan.lua
@@ -114,13 +114,13 @@ local TOGGLE_KEY            = Enum.KeyCode.F
 
 -- Velocidade do jogador (studs/s)
 local NORMAL_SPEED          = 16
-local BOOSTED_SPEED         = 65
+local BOOSTED_SPEED         = 35
 
 -- Duração do efeito (segundos)
 local SANDEVISTAN_DURATION  = 3.5
 
 -- Configurações dos clones
-local CLONE_INTERVAL        = 0.04
+local CLONE_INTERVAL        = 0.02
 
 -- ✅ 0 = SEM LIMITE → clones ficam permanentes até o fim do Sandevistan
 -- (todos os clones criados sobrevivem até o deactivate)
