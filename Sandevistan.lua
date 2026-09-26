e36b5e4f205c241dfb7902bf3ced058af29f927a
qawsedrf-12345 <alexandrebarbosaduarte2011@gmail.com>
2026-09-25 22:43:40 -0300

    Atualizar o Sandevistan.lua

--- Sandevistan.lua
+++ Sandevistan.lua
@@ -114,7 +114,7 @@ local TOGGLE_KEY            = Enum.KeyCode.F
 
 -- Velocidade do jogador (studs/s)
 local NORMAL_SPEED          = 16
-local BOOSTED_SPEED         = 28
+local BOOSTED_SPEED         = 20
 
 -- Duração do efeito (segundos)
 local SANDEVISTAN_DURATION  = 3.5
@@ -122,7 +122,7 @@ local SANDEVISTAN_DURATION  = 3.5
 -- Configurações dos clones
 local CLONE_INTERVAL        = 0.05
 local MAX_CLONES            = 70
-local CLONE_TRANSPARENCY    = 0.8
+local CLONE_TRANSPARENCY    = 0
 local CLONE_HIGHLIGHT_FILL  = 0.0
 local CLONE_HIGHLIGHT_LINE  = 0.2
 local CLONE_MATERIAL        = Enum.Material.Neon
@@ -144,7 +144,7 @@ local AUDIO_SWOOSH_END      = 0.7
 
 local AUDIO_MAIN_ID         = "rbxassetid://130840290979991"
 local AUDIO_MAIN_SPEED      = 0.5
-local AUDIO_MAIN_VOLUME     = 1
+local AUDIO_MAIN_VOLUME     = 2
 local AUDIO_MAIN_START      = 1.9
 
 -- FOV kick
